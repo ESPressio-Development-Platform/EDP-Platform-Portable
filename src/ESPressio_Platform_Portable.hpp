77e061a888005dc354914c248bd1768a7897ea7e
Simon Stuart <lakraven@gmail.com>
2026-09-19 22:35:25 +0200

    #2 Export compact AtomicWord8 provider

--- src/ESPressio_Platform_Portable.hpp
+++ src/ESPressio_Platform_Portable.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "concurrency/AtomicWord8Provider.hpp"
 #include "concurrency/AtomicWord32Provider.hpp"
 #include "memory/ByteOperationsProvider.hpp"
 #include "memory/MemoryResourceProvider.hpp"
